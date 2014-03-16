6b316b704fd739ab02ab1cb1063f63bde0aa086b
Derek DiCillo <dddicillo@gmail.com>
2014-03-16 17:08:35 -0400

    Add typeahead functionality

--- minimash.js
+++ minimash.js
@@ -6,9 +6,36 @@ apiSecret : 'a8148b72b0b56153dc5fc7315c7d0780',
 cache : cache
 });
 
-lastfm.artist.getInfo({artist: 'Dave Koz'}, {success: function(data){
-/* Use Data */
-	console.log(data.bio);
-    }, error: function(code, message){
-    /* Show error message. */
- }});
+lastfm.artist.search({artist: 'Dave Koz'}, {success: function(data){
+	console.log(data.results.artistmatches.artist[0].name);
+}, error: function(code, message) {
+	/* Show error message. */
+}});
+
+var engine = new Bloodhound({
+  name: 'artists',
+  remote: {
+  	url: 'http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=%QUERY&api_key=fbd77c792fee9294d1706b5a65d1cb78&format=json',
+  	filter: function(artists) {
+  		return $.map(artists.results, function (artist) {
+                return {
+                    value: artist.name
+                };
+            });
+        }
+  	},
+  datumTokenizer: function(d) { 
+      return Bloodhound.tokenizers.whitespace(d.val); 
+  },
+  queryTokenizer: Bloodhound.tokenizers.whitespace
+});
+
+$('.typeahead').typeahead(null, {
+	source: engine.ttAdapter(),
+});
+
+// lastfm.artist.getInfo({artist: 'Dave Koz'}, {success: function(data){
+// 		console.log(data.artist.bio.content);
+//     }, error: function(code, message) {
+//     /* Show error message. */
+// }});
