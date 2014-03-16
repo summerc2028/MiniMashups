068e3e4ed7f775ea8449e092dd9edcc45dc740bc
Derek DiCillo <dddicillo@gmail.com>
2014-03-16 17:47:14 -0400

    mess around with typeahead

--- minimash.js
+++ minimash.js
@@ -6,18 +6,19 @@ apiSecret : 'a8148b72b0b56153dc5fc7315c7d0780',
 cache : cache
 });
 
-lastfm.artist.search({artist: 'Dave Koz'}, {success: function(data){
-	console.log(data.results.artistmatches.artist[0].name);
-}, error: function(code, message) {
-	/* Show error message. */
-}});
+// lastfm.artist.search({artist: 'Dave Koz'}, {success: function(data){
+// 	console.log(data.results.artistmatches.artist[0].name);
+// }, error: function(code, message) {
+// 	/* Show error message. */
+// }});
 
 var engine = new Bloodhound({
   name: 'artists',
   remote: {
   	url: 'http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=%QUERY&api_key=fbd77c792fee9294d1706b5a65d1cb78&format=json',
-  	filter: function(artists) {
-  		return $.map(artists.results, function (artist) {
+  	filter: function(data) {
+  		return $.map(data.results.artistmatches.artist, function(artist) {
+  				console.log(artist);
                 return {
                     value: artist.name
                 };
@@ -30,15 +31,17 @@ var engine = new Bloodhound({
   queryTokenizer: Bloodhound.tokenizers.whitespace
 });
 
+engine.initialize();
+
 $('.typeahead').typeahead(null, {
 	source: engine.ttAdapter(),
 });
 
-document.getElementById("submit").addEventListener("click",function(){
-var option = $('#option').val();
-var input = $('#input');
-window.alert("hello");
-},false);
+// document.getElementById("submit").addEventListener("click",function(){
+// var option = $('#option').val();
+// var input = $('#input');
+// window.alert("hello");
+// },false);
 
 // lastfm.artist.getInfo({artist: 'Dave Koz'}, {success: function(data){
 // /* Use Data */
