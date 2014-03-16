a4889b8e5f41dcb2c596a4ac889172572c9559f7
bdagostino <dagostino.27@osu.edu>
2014-03-16 17:03:32 -0400

    Changed HTML

--- minimash.js
+++ minimash.js
@@ -6,9 +6,16 @@ apiSecret : 'a8148b72b0b56153dc5fc7315c7d0780',
 cache : cache
 });
 
+document.getElementById("submit").addEventListener("click",function(){
+var option = $('#option').val();
+var input = $('#input');
+window.alert("hello");
+},false);
+
 lastfm.artist.getInfo({artist: 'Dave Koz'}, {success: function(data){
 /* Use Data */
-	console.log(data.bio);
+	console.log(data);
     }, error: function(code, message){
     /* Show error message. */
  }});
+
