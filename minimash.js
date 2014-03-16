d8d8ad77e545ad7df253826c13e6e90282b33a5b
bdagostino <dagostino.27@osu.edu>
2014-03-16 17:35:07 -0400

    Added Submit Buttton Functions

--- minimash.js
+++ minimash.js
@@ -1,9 +1,18 @@
+$(document).ready(function(){
+
 var cache = new LastFMCache();
 
 var lastfm = new LastFM({
 apiKey : 'fbd77c792fee9294d1706b5a65d1cb78',
 apiSecret : 'a8148b72b0b56153dc5fc7315c7d0780',
 cache : cache
+});
+
+$('#submit').click(function(){
+	var option = $('#option').val();
+	var text = $('#input').val();
+	
+
 });
 
 lastfm.artist.search({artist: 'Dave Koz'}, {success: function(data){
@@ -34,15 +43,10 @@ $('.typeahead').typeahead(null, {
 	source: engine.ttAdapter(),
 });
 
-document.getElementById("submit").addEventListener("click",function(){
-var option = $('#option').val();
-var input = $('#input');
-window.alert("hello");
-},false);
-
 // lastfm.artist.getInfo({artist: 'Dave Koz'}, {success: function(data){
 // /* Use Data */
 // 	console.log(data);
 //     }, error: function(code, message){
 //     /* Show error message. */
 //  }});
+});
