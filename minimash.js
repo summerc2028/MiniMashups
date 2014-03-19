3a45cc1325a88ce5c1c3aca8a41e1d4f22a58f54
bdagostino <dagostino.27@osu.edu>
2014-03-19 14:28:06 -0400

    Added Google Maps API

--- minimash.js
+++ minimash.js
@@ -39,4 +39,17 @@ $(document).ready(function() {
 		var option = $('#option').val();
 		var text = $('#input').val();
 	});
+
 });
+
+/*Google Maps API */
+google.maps.event.addDomListener(window, 'load', initialize);
+function initialize() {
+  	var mapOptions = {
+    	zoom: 8,
+    	center: new google.maps.LatLng(-34.397, 150.644)
+  	};
+
+  	var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+	}
